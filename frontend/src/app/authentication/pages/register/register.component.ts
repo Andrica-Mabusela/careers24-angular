d75968082da04505f751f44a5ae59cd8eaf0197e
Andrica-Mabusela <programmerandy420@gmail.com>
2022-05-26 21:06:58 +0200

    added dates

--- frontend/src/app/authentication/pages/register/register.component.ts
+++ frontend/src/app/authentication/pages/register/register.component.ts
@@ -13,10 +13,19 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
       let baseYear = 1930
       let currentYear = new Date().getFullYear()
-      for(let index = 0; baseYear <= currentYear; index++ ){
-        this.years.push(baseYear)
-        baseYear += 1
+      let difference = currentYear - baseYear
+
+      for(let index = 0; index <= difference; index++) {
+          if( baseYear <= currentYear ){
+              this.years.push(baseYear)
+              baseYear++
+          } else{
+            break
+          }
       }
+
+      this.years = this.years.reverse()
+      console.log(this.years)
   }
 
 }
