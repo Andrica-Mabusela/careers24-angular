63b9a7837481ba02c96ca2342ed87aabf346620c
Andrica-Mabusela <programmerandy420@gmail.com>
2022-05-28 23:52:16 +0200

    experience btns toggler

--- frontend/src/app/authentication/pages/register/register.component.ts
+++ frontend/src/app/authentication/pages/register/register.component.ts
@@ -9,10 +9,12 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
   years: number[] = []
-  seekerButtons: any;
+  seekerButtons: any
+  hasExperienceArray: any
 
   ngOnInit(): void {
     this.seekerButtons = document.querySelectorAll('.seeker-btn')
+    this.hasExperienceArray = document.querySelectorAll('.experience-btn')
       let baseYear = 1930
       let currentYear = new Date().getFullYear()
       let difference = currentYear - baseYear
@@ -36,10 +38,39 @@ export class RegisterComponent implements OnInit {
       this.seekerButtons.forEach( (btn: any) => {
           if(btn == event.target) {
             btn.classList.add('employment-active')
+
+            if(btn.classList.contains('employed-btn')) {
+              (document.getElementById('employed') as HTMLDivElement).style.display = 'block';
+              (document.getElementById('unemployed') as HTMLDivElement).style.display = 'none';
+              (document.getElementById('first-time-seeker') as HTMLDivElement).style.display = 'none'
+
+            } else if( btn.classList.contains('unemployed-btn') ){
+
+              (document.getElementById('employed') as HTMLDivElement).style.display = 'none';
+              (document.getElementById('unemployed') as HTMLDivElement).style.display = 'block';
+              (document.getElementById('first-time-seeker') as HTMLDivElement).style.display = 'none'
+            } else {
+              (document.getElementById('employed') as HTMLDivElement).style.display = 'none';
+              (document.getElementById('unemployed') as HTMLDivElement).style.display = 'none';
+              (document.getElementById('first-time-seeker') as HTMLDivElement).style.display = 'block'
+
+            }
+
           } else {
             btn.classList.remove('employment-active')
           }    
       })
+  }
+
+
+  experienceToggler(event: Event) {
+    this.hasExperienceArray.forEach((btn: any) => {
+        if( btn == event.target ){
+            btn.classList.add('experience-active')
+        } else {
+            btn.classList.remove('experience-active')
+        }
+    })
   }
 
 }
