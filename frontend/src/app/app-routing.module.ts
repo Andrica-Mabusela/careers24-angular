d724a6456d6c64a5eea8bfc5b4bca74a63e46af7
Andrica-Mabusela <programmerandy420@gmail.com>
2022-06-07 22:47:31 +0200

    start home page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/pages/login/login.component';
 import { RegisterComponent } from './authentication/pages/register/register.component';
 import { ForgotPasswordComponent } from './authentication/pages/forgot-password/forgot-password.component'
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent },
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'forgotpassword', component: ForgotPasswordComponent}
